3e56c0808021c4f3c291037a34c6a9c9890accf6
rakib-anam <52469465+rakib-anam@users.noreply.github.com>
2023-02-15 17:24:33 -0500

    changed to pokemon card

--- src/App.js
+++ src/App.js
@@ -5,23 +5,32 @@ function App() {
   return (
 
 <>
-    <div class="outline">
-      <div class="lecard">
-        <h2>Some Nerd</h2>
-        <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
-        <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
-      </div>
-    </div>
-  <div class="buttons">
+
+<div class="buttons">
+  <button class="duplicateButton">Duplicate</button>
+  <button class="deleteButton">Delete</button>
+  <button class="changeNameButton">Change Name</button>
+</div>
+
+<div class="wrapper">
   
-  <button class="copyCat">Copy</button>
-  <button id="del">Delete</button>
-  <button class="btnDet">Toggle Details</button> 
-  <button class="btnCT">Change Title</button>
-  <button  class="btnBG">Change Background Color</button>
+    <img class="image" src="https://cdn.dribbble.com/users/902228/screenshots/5813718/media/539d18b06ff32a69bb74f95b78cf6602.jpg?compress=1&resize=400x300"/>
   
+  <div class="header">
+    <h1>Charizard</h1>
   </div>
   
+  <div class="details">
+    <details>
+      <summary>About this Pokemon</summary>
+      <p>
+        Charizard is a Fire/Flying type Pokémon introduced in Generation 1. It is known as the Flame Pokémon. Charizard has two Mega Evolutions available in its X & Y forms. In in Mega evolved form, Charizard is arguably the most powerful pokemon
+      </p>
+    </details>
+    
+  </div>
+  
+</div>  
 </>
   );
 }
